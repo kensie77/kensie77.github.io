bf7e921fbbcc15cd9a346657354d93a649facf31
kensie77 <97555224+kensie77@users.noreply.github.com>
2022-04-21 13:33:33 -0600

    assignment 14

---
+++ class practice/week14-demo.js
@@ -0,0 +1,20 @@
+const myCar = new Object();
+myCar.color= "Gray";
+myCar.make= "Mazda";
+myCar.model="CX-5";
+myCar.year="2017";
+
+
+// general syntax: onject.property =value;
+
+console.log(myCar.make);
+
+// a second wya of defing an object
+const mySecondCar= {
+  color:'Blue',
+  make:'Ford',
+  model:'Ranger',
+  year:'2020'
+}
+
+console.log(mySecondCar.make);
